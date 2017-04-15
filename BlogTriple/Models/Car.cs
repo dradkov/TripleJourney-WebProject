7fb96e7c6cc46394dd53fd7e21dcba548d8f1536
mirostanchev <mirostanchev@abv.bg>
2017-04-15 20:23:28 +0300

    Added Car Model and fixed footer routes

---
+++ BlogTriple/Models/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogTriple.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        
+        public string Location { get; set; }
+
+        public DateTime PickUpDate { get; set; }
+
+        public DateTime DropOffDate { get; set; }
+
+        public DateTime PickUpTime { get; set; }
+
+        public DateTime DropOffTime { get; set; }
+    }
+}
