25d611da07a6294a951f43055a11e60f80f6d49f
mirostanchev <mirostanchev@abv.bg>
2017-04-18 00:23:48 +0300

    Added Rent a Car functionality

--- BlogTriple/Controllers/CarsController.cs
+++ BlogTriple/Controllers/CarsController.cs
@@ -3,15 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BlogTriple.Models;
 
 namespace BlogTriple.Controllers
 {
     public class CarsController : Controller
     {
-        // GET: Cars
+        [HttpGet]
         public ActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                var db = new BlogDbContext();
+
+                db.Cars.Add(car);
+                db.SaveChanges();
+
+                return RedirectToAction("Details", new { id = car.Id });
+            }
+
+            return View(car);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var db = new BlogDbContext();
+
+            var car = db.Cars.Where(c => c.Id == id).FirstOrDefault();
+
+            if (car == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(car);
         }
     }
 }
--- BlogTriple/Models/BlogDbContext.cs
+++ BlogTriple/Models/BlogDbContext.cs
@@ -21,6 +21,8 @@ namespace BlogTriple.Models
 
         public virtual IDbSet<HotelsViewModel> Destinations { get; set; }
 
+        public virtual IDbSet<Car> Cars { get; set; }
+
         public static BlogDbContext Create()
         {
             return new BlogDbContext();
