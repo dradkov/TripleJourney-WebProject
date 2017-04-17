20fac63243ef5427396f46f3ce07d39916d6bcee
mirostanchev <mirostanchev@abv.bg>
2017-04-17 21:15:18 +0300

    Change Car Model and added Rent a Car View

--- BlogTriple/Models/Car.cs
+++ BlogTriple/Models/Car.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,15 +9,29 @@ namespace BlogTriple.Models
     public class Car
     {
         public int Id { get; set; }
-        
+
+        [Required]
+        [Display(Name = "Location")]
         public string Location { get; set; }
 
+        [Required]
+        [Display(Name = "Pick-up Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime PickUpDate { get; set; }
 
+        [Required]
+        [Display(Name = "Drop-off Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DropOffDate { get; set; }
 
+        [Required]
+        [Display(Name = "Pick-up Time")]
+        [DataType(DataType.Time)]
         public DateTime PickUpTime { get; set; }
 
+        [Required]
+        [Display(Name = "Drop-off Time")]
+        [DataType(DataType.Time)]
         public DateTime DropOffTime { get; set; }
     }
 }
