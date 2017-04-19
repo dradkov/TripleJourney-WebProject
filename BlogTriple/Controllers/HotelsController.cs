db9a4818cf8269bfb03d7158e487abffb5df1f42
dradkov <dimitar_radkov@abv.bg>
2017-04-19 15:41:20 +0300

    Add Function on HotelsController Fixing Route at Details /Action

--- BlogTriple/Controllers/HotelsController.cs
+++ BlogTriple/Controllers/HotelsController.cs
@@ -1,6 +1,7 @@
 ﻿using BlogTriple.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,11 +10,6 @@ namespace BlogTriple.Controllers
 {
     public class HotelsController : Controller
     {
-        // GET: Hotels
-        public ActionResult Index()
-        {
-            return View();
-        }
 
         [HttpGet]
         [Authorize]
@@ -26,23 +22,57 @@ namespace BlogTriple.Controllers
         [Authorize]
         public ActionResult Destination(HotelsViewModel city)
         {
-            var database = new BlogDbContext();
 
             if (ModelState.IsValid)
             {
+                var database = new BlogDbContext();
+
+                var startDate = new DateTime(city.From.Year, city.From.Month, city.From.Day);
+                var endDate = new DateTime(city.To.Year, city.To.Month, city.To.Day);
+
+                var numDays = endDate.Subtract(startDate);
+                var convertDays = numDays.TotalDays;
+
+                decimal result = 0;
+                if (city.Rooms == "1")
+                {
+                    result = (decimal)convertDays * 20;
+                }
+                else if (city.Rooms == "2")
+                {
+                    result = (decimal)convertDays * 30;
+                }
+                else if (city.Rooms == "3")
+                {
+                    result = (decimal)convertDays * 40;
+                }
+                else if (city.Rooms == "4")
+                {
+                    result = (decimal)convertDays * 60;
+                }
+
+                city.Price = result;
                 database.Destinations.Add(city);
                 database.SaveChanges();
 
-                return RedirectToAction("Details", "Hotels");
+                return RedirectToAction("DestinationDetails", new { id = city.Id });
             }
             return View();
         }
 
-        [HttpGet]
-        [Authorize]
-        public ActionResult Details()
+
+        public ActionResult DestinationDetails(int? id)
         {
-            return View();
+            var database = new BlogDbContext();
+
+            var city = database.Destinations.Where(c => c.Id == id).FirstOrDefault();
+
+            if (city == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(city);
         }
 
 
